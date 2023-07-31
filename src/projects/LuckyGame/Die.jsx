010ed32e482427c6b14f35cb60f464b370b86074
Josiah ENUMAH <josiahenumah94@gmail.com>
2023-07-31 10:18:46 +0100

    Lucky Game

---
+++ src/projects/LuckyGame/Die.jsx
@@ -0,0 +1,9 @@
+import "../all.css"
+
+const Die = ({val, color = "slateblue"}) => {
+    return (<div className="Die" style={{backgroundColor: color}}>
+                {val}
+            </div>)
+};
+
+export default Die;
